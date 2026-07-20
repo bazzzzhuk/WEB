735623a05ef6b48d1481c3c80e1727ab281e7b48
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-07-20 21:25:29 +0300

    factorial todo result

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -3,7 +3,8 @@ function Factorial() {
 	let numberElement = document.getElementById("number");
 	let number = numberElement.value;
 	let resultElement = document.getElementById("factorial-result");
-	let f = 1;
-	for (let i = 1; i <= number; i++) { f *= i };
-	resultElement.innerHTML = f;
+	let f = BigInt(1);
+	for (let i = 1n; i <= number; i++) { f *= i };
+	resultElement.innerHTML = `${number}! = ${f}`;
+	//resultElement.value = `${number}! = ${f}`;
 }
