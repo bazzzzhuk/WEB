60bf6abfbbcffe3537eb20ac1b544c7880b232e6
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-07-22 00:29:38 +0300

    HW Done

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -1,10 +1,32 @@
 // JavaScript source code
+charset = "utf-8"
 function Factorial() {
 	let numberElement = document.getElementById("number");
 	let number = numberElement.value;
-	let resultElement = document.getElementById("factorial-result");
+	let resultElement = document.getElementById("result");
 	let f = BigInt(1);
 	for (let i = 1n; i <= number; i++) { f *= i };
 	resultElement.innerHTML = `${number}! = ${f}`;
 	//resultElement.value = `${number}! = ${f}`;
+}
+function Power()
+{
+	let number_p = document.getElementById("number_p").value;
+	let power_p = document.getElementById("power_p").value;
+	document.getElementById("result_p").innerHTML = `${number_p} ^ ${power_p} = ${BigInt(number_p**power_p)}`;
+}
+function Fibona44i()
+{
+	let end_fibona44i = document.getElementById("number_f").value;
+	let temp_value;
+	let fibona44i_1 = 0;
+	let fibona44i_2 = 1;
+	let vyvod = '0<br>';
+	while (end_fibona44i >= fibona44i_2) {
+		vyvod = vyvod + fibona44i_2 + '<br>';
+		temp_value = fibona44i_2;
+		fibona44i_2 += fibona44i_1;
+		fibona44i_1 = temp_value;
+	}
+	document.getElementById("result_f").innerHTML = `${vyvod} <br> ${fibona44i_2} < следующее число в последовательности Фибоначчи больше введенного предела в ${end_fibona44i}!`
 }
