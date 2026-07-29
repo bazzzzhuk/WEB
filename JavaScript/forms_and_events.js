d2b4193d05a60d5d87e8a402388e49dc806fa7c4
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-07-29 21:25:47 +0300

    Timer in progress

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -129,3 +129,47 @@ function tickTimer()
 	setTimeout(tickTimer, 100);
 }
 tickTimer();
+
+document.getElementById('btn-start').addEventListener("click", startCountdownTimer);
+function startCountdownTimer()
+{
+	let targetDate	= document.getElementById("target-date");
+	let targetTime	= document.getElementById("target-time");
+	let btnStart = document.getElementById("btn-start");
+	if (btnStart.value === "Start") {
+		btnStart.value = "Stop";
+		targetDate.disabled = targetTime.disabled = true;
+		tickCountdown();
+	}
+	else
+	{
+		btnStart.value = "Start";
+		targetDate.disabled = targetTime.disabled = false;
+	}
+
+}
+function tickCountdown()
+{
+	let now = new Date();
+
+	let targetDateControl = document.getElementById("target-date");
+	let targetTimeControl = document.getElementById("target-time");
+
+	let targetDateValue = targetDateControl.valueAsDate;
+	let targetTimeValue = targetTimeControl.valueAsDate;
+	//Выравниваем часовой пояс:
+	targetDateValue.setHours(targetDateValue.getHours() + targetDateValue.getTimezoneOffset() / 60);
+	targetTimeValue.setHours(targetTimeValue.getHours() + targetTimeValue.getTimezoneOffset() / 60);
+
+	targetTimeValue.setFullYear(targetDateValue.getFullYear());
+	targetTimeValue.setMonth(targetDateValue.getMonth());
+	targetTimeValue.setDate(targetDateValue.getDate());
+
+	let duration = targetTimeValue - now;
+	document.getElementById("duration").innerHTML = duration;
+
+	document.getElementById("target-date-value").innerHTML = targetDateValue;
+	document.getElementById("target-time-value").innerHTML = targetTimeValue;
+
+	setTimeout(tickCountdown, 100);
+}
